8c3149276e446ef52edd9c543f42ca02f2bee59b
chezynick <chezynickannie@yahoo.co.uk>
2020-10-06 11:39:18 +0100

    working version of game

--- app.js
+++ app.js
@@ -1,51 +1,97 @@
-//create a gameboard array with 9 empty values all with the class of 'space' 
-const gameBoard = new Array();
-function makeBoard  ()  {
-    
-    for(let i=0;i<9;i++){
-        const text = ''
-       
-        gameBoard.push(text)
-        }        
-  renderBoard()      
-};
-makeBoard();
- 
 //create a player constructor with takeTurn function
-const playerMaker = (name, icon) => {
+const PlayerMaker = (name, icon) => {
     const takeTurn = () => {
         let board = document.querySelectorAll('.space');
          for(let i=0;i<board.length;i++){
         board[i].addEventListener('click', placeIcon)
         function placeIcon (){
-        //    gameBoard[i].push(icon);
-                board[i].innerHTML = icon;
+           gameBoard[i] = icon
+                renderBoard()     
             }
+            
         }
   };
     return { name, icon, takeTurn };
 };
-//create the 2 players player a/player b for now 
-const player1 = playerMaker('Player 1', 'x');
-const player2 = playerMaker('Player 2', 'o');
 
+
+//create a gameboard array with 9 empty values all with the class of 'space' 
+let gameBoard = new Array();
+
+//create the 2 players player a/player b for now 
+        const player1 = PlayerMaker('Player 1', 'x');
+        const player2 = PlayerMaker('Player 2', 'o');
+        
+//call makeBoard to start game
+makeBoard();
+
+function makeBoard  ()  {
+
+    for(let i=0;i<9;i++){
+        const text = ' '
+        gameBoard.push(text)
+        }          
+  renderBoard()     
+};
+
+ 
 //render the board
 function renderBoard() {
     let emptyBoard = document.getElementById('board');
-    
+    board.innerHTML = '';
     for(let i =0;i<gameBoard.length;i++){
         let box = document.createElement('div');
         box.classList.add('space');
         box.innerHTML = gameBoard[i];
         emptyBoard.appendChild(box);
-    }
-    
-        
+    }  
+    nextTurn(); 
     };
 
 
 //an object to control the flow of the game eg. with each turn a click will change the array to
 //if last x = o then next symbol will be X or if O<X next symbol will be O
 //X or O and return to render the board
+function nextTurn(){
+    
+    if (gameBoard[0] + gameBoard[1] + gameBoard[2] === 'xxx' || gameBoard[3] + gameBoard[4] + gameBoard [5] === 'xxx' 
+        ||gameBoard[6] + gameBoard[7] + gameBoard[8] === 'xxx' || gameBoard[0] +gameBoard[3] + gameBoard [6] === 'xxx' 
+        ||gameBoard[1]+ gameBoard[4] + gameBoard[7] === 'xxx' || gameBoard[2] +gameBoard[5]+gameBoard[8] === 'xxx'
+        ||gameBoard[0]+ gameBoard[4]+gameBoard[8] === 'xxx' || gameBoard[2] + gameBoard[4] + gameBoard[6] === 'xxx') {
+        winner(player1)
+        } if (gameBoard[0] + gameBoard[1] + gameBoard[2] === 'ooo' || gameBoard[3] + gameBoard[4] + gameBoard [5] === 'ooo' 
+        ||gameBoard[6] + gameBoard[7] + gameBoard[8] === 'ooo' || gameBoard[0] +gameBoard[3] + gameBoard [6] === 'ooo' 
+        ||gameBoard[1]+ gameBoard[4] + gameBoard[7] === 'ooo' || gameBoard[2] +gameBoard[5]+gameBoard[8] === 'ooo'
+        ||gameBoard[0]+ gameBoard[4]+gameBoard[8] === 'ooo' || gameBoard[2] + gameBoard[4] + gameBoard[6] === 'ooo') {
+        winner(player2);
+        } else {
+    let howMany = gameBoard.filter(a => a === ' ');
+    if (howMany.length === 0){
+        alert('No winner this time')
+        gameBoard = []
+        makeBoard();
+    } else if((howMany.length % 2) === 0){
+        player2.takeTurn();
+    } else{
+        player1.takeTurn();
+    };
+};
+};
 
-//function to determine if a win has been achieved if not repeat
+//winner function with play again option
+const winner = (a) => {
+    //select winnerBox from HTML
+    let winBox = document.getElementById('winnerBox');
+    let winName = document.getElementById('player');
+    let nextGame = document.getElementById('button');
+    //change text on player
+    winName.innerText = a.name;
+    //change display on winnerBox
+    winBox.style.display = 'flex';
+    nextGame.addEventListener('click', () => {
+        winBox.style.display = 'none'
+        gameBoard = []
+        makeBoard();
+    });
+
+};
